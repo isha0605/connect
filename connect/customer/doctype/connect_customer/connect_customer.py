0ef332df6b663f4495b2c053ad910b6e09b80d88
Maitri Doshi <doshi.maitri.mk@gmail.com>
2026-08-28 09:11:59 +0000

    chore(connect): delete unused Connect Customer and Connect Customer Member doctypes
    Their data was already migrated to Customer and Customer Team Member.
    Nothing in the app reads or writes these two anymore.

--- connect/customer/doctype/connect_customer/connect_customer.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Isha and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ConnectCustomer(Document):
-	pass
